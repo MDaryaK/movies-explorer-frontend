be4ac8cf572d0981205715b0d68a65e2f3ed79e4
Artyom Petrunin <119070481+MDaryaK@users.noreply.github.com>
2023-11-01 17:50:43 +0300

    fixes

--- src/components/Movies/MoviesCard/index.js
+++ src/components/Movies/MoviesCard/index.js
@@ -1,13 +1,13 @@
 import "./index.css";
 
-export default function MoviesCard({ name, time, img }) {
+export default function MoviesCard({ name, link, time, img }) {
   return (
-    <div className="movies-card">
+    <a className="movies-card" target="_blank" href={link}>
       <div className="movies-card__head">
         <h2 className="movies-card__head-name">{name}</h2>
         <p className="movies-card__head-time">{time}</p>
       </div>
       <img className="movies-card__preview" src={img} alt={name} />
-    </div>
+    </a>
   );
 }
--- src/components/Movies/MoviesList/index.js
+++ src/components/Movies/MoviesList/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 import formatDuration from "../../../functions/formatDuration";
 
 export default function MoviesList({ data }) {
+  console.log(data);
   return (
     <div className="movies__list">
       {data && data.map((item) => {
@@ -12,6 +13,7 @@ export default function MoviesList({ data }) {
           <MoviesCard
             key={item.id}
             name={item.nameRU}
+            link={item.trailerLink}
             time={`${hours}ч ${minutes > 0 ? `${minutes}м` : ''}`}
             img={`https://api.nomoreparties.co/${item.image.url}`}
           />
